a2d58c1c1042d3e1c7e3c00ae547b5d3eba460ab
Marisa Raeder <mraeder11@gmail.com>
2020-09-01 08:56:10 -0500

    Functional Components

--- src/components/CampaignInfoComponent.js
+++ src/components/CampaignInfoComponent.js
@@ -1,52 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampaignInfo extends Component {
-    
-    renderProgress(progress) {
-        if(progress) {
-            return(
-                <div className="col-md-5 m-1">
-                    <h4>Progress</h4>
-                    {progress.map(progress => {
-                        return (
-                            <div key={progress.id}>
-                                <p>{progress.text} </p>
-                                <p>{progress.percent} </p>
-                            </div>
-                        );
-                    })}
-                </div>
-            )
-        }
-        return <div/>;
+function RenderCampaign({campaign}) {
+     
+    return (
+        <div className="col-md-5 m-1">             
+            <Card>
+                <CardImg top src={campaign.image} alt={campaign.name} />
+                <CardBody>
+                    <CardTitle>{campaign.name}</CardTitle>
+                    <CardText>{campaign.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+ 
+function renderProgress({progress}) {
+    if(progress) {
+        return(
+            <div className="col-md-5 m-1">
+                <h4>Progress</h4>
+                {progress.map(progress => {
+                    return (
+                        <div key={progress.id}>
+                            <p>{progress.text} </p>
+                            <p>{progress.percent} </p>
+                        </div>
+                    );
+                })}
+            </div>
+        )
     }
+    return <div/>;
+}
     
-    renderCampaign(campaign) {
+function CampaignInfo(props) {
+    if (props.campaign) {
         return (
-            <div className="col-md-5 m-1">             
-                <Card>
-                    <CardImg top src={campaign.image} alt={campaign.name} />
-                    <CardBody>
-                        <CardTitle>{campaign.name}</CardTitle>
-                        <CardText>{campaign.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>)
-    }
-    render() {
-        if (this.props.campaign){
-            return (
-                <div className="container">
-                    <div className="row">
-                        {this.renderCampaign(this.props.campaign)}
-                        {this.renderProgress(this.props.campaign.progress)}
-                    </div>
+            <div className="container">
+                <div className="row">
+                    <RenderCampaign campaign={props.campaign} />
+                    <RenderProgress progress={props.campaign.progress} />
                 </div>
-            );
-        }
-        return <div/>;
+            </div>
+        );
     }
+    return <div />;
 }
 
 export default CampaignInfo;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Directory extends Component {  
+function RenderDirectoryItem ({campaign, onClick}) {
+    return (
+        <Card onClick={() => onClick(campaign.id)}>
+            <CardImg width="100%" src={campaign.image} alt={campaign.name} />
+            <CardImgOverlay>
+                <CardTitle>{campaign.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
 
-    render() {
-        const directory = this.props.campaigns.map ( campaign => {
-            return (
-                <div key={campaign.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(campaign.id)}>
-                        <CardImg width="100%" src={campaign.image} alt={campaign.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campaign.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
+function Directory (props) {
 
+    const directory = props.campaign.map(campaign => {
         return (
-            <div className="container">
-                <div className="row">
-                {directory}
-                </div>
-                {/* <div className="col-md-5  m-1">
-                    {this.renderSelectedCampaign(this.state.selectedCampaign)}
-                </div> */}
+            <div key={campaign.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campaign={campaign} onClick={props.onClick} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
 }
 
 export default Directory;
